3add13b5db2cbb3feeac6a8f77c09fc1f86c12c0
Ihor <ihor.bielkov@tul.cz>
2019-03-22 15:48:20 +0100

    added CNB

---
+++ Igor_a_Daniel/src/igor_a_daniel/bank/CNB/downloadFile.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package igor_a_daniel.bank.CNB;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author Игорь
+ */
+public class downloadFile {
+
+    private static LocalDate date = LocalDate.now();
+
+    public static void downloadFileCurrenciesCNB() {
+        try {
+
+            DateTimeFormatter formatters = DateTimeFormatter.ofPattern("dd.MM.uuuu");
+
+            String text = date.format(formatters);
+            URL website = new URL("http://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.txt?date=" + text);
+            ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+            FileOutputStream fos = new FileOutputStream("src/igor_a_daniel/bank/CNB/kurzy.txt");
+            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    private static void convertToCsv() throws IOException {
+        FileWriter writer;
+        File file = new File("kurzy.txt");
+        Scanner scan = new Scanner(file);
+        File file2 = new File("src/igor_a_daniel/bank/CNB/kurz " + date.toString() + ".csv");
+            writer = new FileWriter(file2);
+            Writer fstream = null;
+            BufferedWriter out = null;
+            try {
+                fstream = new OutputStreamWriter(new FileOutputStream(file2), "cp1250");
+                out = new BufferedWriter(fstream);
+                while (scan.hasNext()) {
+
+                    String csv = scan.nextLine().replace("|", ";");
+                    System.out.println(csv);
+                    out.append(csv);
+                    out.append("\n");
+                    out.flush();
+                }
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+
+        }
+    
+}
